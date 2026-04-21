4c3b98a5d90ee2da6b52194650cc9a14d6dbccbf
vannsoko <konstantin.vannson@gmail.com>
2026-04-22 00:27:06 +0200

    update

--- q-table.py
+++ q-table.py
@@ -1,16 +1,22 @@
 from tictactoe_environment import TicTacToeENV
-from collections import defaultdict
 import numpy as np
 import random
 from tqdm import tqdm
 import pickle
+import threading
+import sys
+import array
 # import mathplotlib.pyplot as plt
 # import time
+
+
+# lock = True if sys.maxsize < 2**63-1 else False
 
 env = TicTacToeENV("console")
 testenv = TicTacToeENV("console")
 
-q_table = defaultdict(lambda: np.zeros(9))
+q_table = np.zeros(177148, dtype=np.float64)
+# q_table = array.array('d', 177148 * [0.0]) # Becomes a RawArray for 64 bit architechtures
 
 # 2. Hyperparamètres de l'apprentissage
 alpha = 0.5  # Taux d'apprentissage (vitesse d'assimilation des nouvelles informations)
@@ -37,13 +43,13 @@ def evaluate_agent(eval_env, q_table, nb_episodes=100, epsilon=0):
         state, _ = eval_env.reset()
         done = False
         while not done:
-            s_tuple = tuple(state)
+            state_index = state_to_index(state)
 
             # Exploitation pure : on prend la meilleure action connue
-            if random.uniform(0, 1) < epsilon or s_tuple == np.zeros(9):
+            if random.uniform(0, 1) < epsilon or state_index == 0:
                 action = eval_env.action_space.sample()
             else:
-                action = np.argmax(q_table[s_tuple])
+                action = np.argmax(q_table[state_index: state_index+9])
 
             state, reward, finished, truncated, info = eval_env.step(action)
             done = finished or truncated
@@ -54,77 +60,110 @@ def evaluate_agent(eval_env, q_table, nb_episodes=100, epsilon=0):
     return results
 
 
+def state_to_index(state, action=0):
+    state_value = 0
+
+    # Same idea as a bitmask but with 3 possibilites
+    for i in range(9):
+        state_value += state[i] * (3**i)
+
+
+    # Multiply times 9 to create space for the 9 possible (valid or not) actions per state 
+    state_value *= 9
+
+    state_value += action
+
+
+#   q_table[state_tuple][action] = nouvelle_valeur
+def update_q_table(state: np.array, action: int, value: int):
+    global q_table
+    
+    state_index = state_to_index(state, action)
+    q_table[state_index+action] = value
+
+
+    # TODO update la q_table pour toutes les simetries disponible
+    # rotation
+
+
 print("début de l'entraînement")
 
-for episode in tqdm(range(1, episodes + 1), desc="Entraînement Q-Table"):
-    alpha *= 0.9995
-    state, _ = env.reset()
-    state_tuple = tuple(state)
 
-    termine = False
-    if episode > 2000:
-        epsilon = epsilon * epsilon_decay if epsilon > epsilon_min else epsilon_min
+def func(worker_id, episodes=episodes, alpha=alpha, epsilon=epsilon, gamma=gamma, epsilon_min=epsilon_min, epsilon_decay=epsilon_decay):
+    for episode in tqdm(range(1, episodes + 1), desc=f"Entraînement Q-Table {worker_id=}"):
+        
+        alpha *= 0.9995
+        state, _ = env.reset()
+    
+        termine = False
+        if episode > 2000:
+            epsilon = epsilon * epsilon_decay if epsilon > epsilon_min else epsilon_min
+    
+        while not termine:
+            state_index = state_to_index(state)
 
-    while not termine:
-        if episode < 2000 or random.uniform(0, 1) < epsilon:
-            # valid_actions = [i for i, v in enumerate(state) if v == 0]
-            # action = random.choice(valid_actions)
-            action = env.action_space.sample()
-        else:
-            action = np.argmax(q_table[state_tuple])
-
-        next_state, reward, finished, truncated, info = env.step(action)
-        next_state_tuple = tuple(next_state)
-        termine = finished or truncated
-
-        if next_state_tuple not in q_table:
-            q_table[next_state_tuple] = np.zeros(9)
-
-        # Calcul de la valeur future maximbale
-        # Si la partie est finie, il n'y a plus de futur, donc la valeur est 0
-        if termine:
-            max_future_q = 0
-        else:
-            max_future_q = np.max(q_table[next_state_tuple])
-
-        #  Q(s, a) <- Q(s, a) + alpha [ R + gamma * max_{a'} Q(s', a') - Q(s, a) ]
-
-        # MISE À JOUR DE LA Q-TABLE (Équation de Bellman)
-        ancienne_valeur = q_table[state_tuple][action]
-        nouvelle_valeur = ancienne_valeur + alpha * (
-            reward + gamma * max_future_q - ancienne_valeur
-        )
-        q_table[state_tuple][action] = nouvelle_valeur
-
-        # L'agent passe à l'état suivant
-        state_tuple = next_state_tuple
-
-        # Affichage de la progression
-        if (episode + 1) % 5000 == 0:
-            print(
-                f"Épisode {episode + 1}/{episodes} terminé. Epsilon actuel: {epsilon:.3f}"
+            if episode < 2000 or random.uniform(0, 1) < epsilon:
+                # valid_actions = [i for i, v in enumerate(state) if v == 0]
+                # action = random.choice(valid_actions)
+                valid_actions = [i for i in range(9)]
+                action = env.action_space.sample()
+            else:
+                action = np.argmax(q_table[state_index:state_index+9])
+    
+            next_state, reward, finished, truncated, info = env.step(action)
+            next_state_index = state_to_index(next_state)
+            termine = finished or truncated
+    
+    
+            # Calcul de la valeur future maximbale
+            # Si la partie est finie, il n'y a plus de futur, donc la valeur est 0
+            if termine:
+                max_future_q = 0
+            else:
+                max_future_q = np.max(q_table[next_state_index:next_state_index+9])
+    
+            #  Q(s, a) <- Q(s, a) + alpha [ R + gamma * max_{a'} Q(s', a') - Q(s, a) ]
+    
+            # MISE À JOUR DE LA Q-TABLE (Équation de Bellman)
+            ancienne_valeur = q_table[state_index + action]
+            nouvelle_valeur = ancienne_valeur + alpha * (
+                reward + gamma * max_future_q - ancienne_valeur
             )
+        
+            # q_table[state_index + action] = nouvelle_valeur
+            update_q_table(state, action, nouvelle_valeur)
+    
+            # L'agent passe à l'état suivant
+            state = next_state
+    
+            # Affichage de la progression
+            if (episode + 1) % 5000 == 0:
+                print(
+                    f"Épisode {episode + 1}/{episodes} terminé. Epsilon actuel: {epsilon:.3f}"
+                )
+    
+        #  print(
+        #      f"\nEntraînement terminé ! L'agent a exploré {len(q_table)} états différents du plateau."
+        #  )
+    
+        if (episode) % 500 == 0 or episode == 1:
+            results = evaluate_agent(testenv, q_table, 100, 0)
+            epsilon_results = evaluate_agent(testenv, q_table, 100, 0.3)
+            print("-" * 20)
+            print(f"epsilon: {epsilon}")
+            print(
+                f"results:\nwins: {results.get(1)}%\ndraws: {results.get(0)}%\n losses: {results.get(-1)}%\n illegal: {results.get(-100)}%"
+            )
+            print("-" * 10)
+            print(
+                f"epsilon_results:\nwins: {epsilon_results.get(1)}%\ndraws: {epsilon_results.get(0)}%\n losses: {epsilon_results.get(-1)}%\n illegal: {epsilon_results.get(-100)}%"
+            )
+            print("-" * 20)
+            with open(f"tictactoe_q_table_{episode}.pkl", "wb") as file:
+                pickle.dump(q_table, file)
 
-    #  print(
-    #      f"\nEntraînement terminé ! L'agent a exploré {len(q_table)} états différents du plateau."
-    #  )
 
-    if (episode) % 500 == 0 or episode == 1:
-        results = evaluate_agent(testenv, q_table, 100, 0)
-        epsilon_results = evaluate_agent(testenv, q_table, 100, 0.3)
-        print("-" * 20)
-        print(f"epsilon: {epsilon}")
-        print(
-            f"results:\nwins: {results.get(1)}%\ndraws: {results.get(0)}%\n losses: {results.get(-1)}%\n illegal: {results.get(-100)}%"
-        )
-        print("-" * 10)
-        print(
-            f"epsilon_results:\nwins: {epsilon_results.get(1)}%\ndraws: {epsilon_results.get(0)}%\n losses: {epsilon_results.get(-1)}%\n illegal: {epsilon_results.get(-100)}%"
-        )
-        print("-" * 20)
-        with open(f"tictactoe_q_table_{episode}.pkl", "wb") as file:
-            pickle.dump(q_table, file)
-
+func()
 print("Training complete! Saving the agent...")
 
 # Open a file in 'wb' (write binary) mode
